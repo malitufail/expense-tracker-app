beb11a6b3879976d1439a06a982e23d66a604544
Muhammad Ali Tufail <m.alitufail@hotmail.com>
2023-07-06 15:56:31 +0500

    get all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,8 @@
 const userController = require('../controllers/user.controller')
 
 module.exports = (app) => {
-    app.get("/api/users/:id", userController.getUserById);
-    app.get("/api/users/all", userController.getAllUsers);
+    app.get("/api/users/get-user-by-id/:id", userController.getUserById);
+    app.get("/api/users/alls", userController.getAllUsers);
     app.post("/api/users/register", userController.registerUser);
     app.post('/api/users/login', userController.loginUser);
 }
